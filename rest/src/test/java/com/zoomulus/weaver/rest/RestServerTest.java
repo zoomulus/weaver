1fc195d5b511fe2a51ffd6229bd22fe98224021a
Matt Ryan <matt@mvryan.org>
2014-11-28 09:41:41 -0700

    Add type conversion tests for MatrixParam.

--- rest/src/test/java/com/zoomulus/weaver/rest/RestServerTest.java
+++ rest/src/test/java/com/zoomulus/weaver/rest/RestServerTest.java
@@ -93,7 +93,6 @@ public class RestServerTest
         verifyOkResult(new RequestResult("get/typematch/int/4000000"), "4000000");
     }
     
-    @Ignore
     @Test
     public void testGetIntWithFloatValueFails() throws ClientProtocolException, IOException
     {
@@ -132,11 +131,10 @@ public class RestServerTest
         assertEquals(Double.valueOf(doubleVal), Double.valueOf(rr.content()));
     }
     
-    @Ignore
     @Test
     public void testGetDoubleWithIntValueConverts() throws ClientProtocolException, IOException
     {
-        verifyOkResult(new RequestResult("get/typematch/double/13579"), "13579");
+        verifyOkResult(new RequestResult("get/typematch/double/13579"), "13579.0");
     }
     
     @Test
@@ -194,7 +192,7 @@ public class RestServerTest
     @Test
     public void testGetMatrixParamRepeatedMatchesReturnsLast() throws ClientProtocolException, IOException
     {
-        verifyOkResult(new RequestResult("get/matrix/multiple/rep1;var=alice/rep2;var=bob"), "var:bob");
+        verifyOkResult(new RequestResult("get/matrix/multiple/rep/rep1;var=alice/rep2;var=bob"), "var:bob");
     }
     
     @Test
@@ -203,6 +201,94 @@ public class RestServerTest
         // This is how MatrixParam should behave; if you want to split the whole string
         // you have to use PathSegment and do it yourself, you big baby
         verifyOkResult(new RequestResult("get/matrix/single/12345;name=bob&age=30&home=Nowhere"), "id:12345,name:bob&age=30&home=Nowhere");
+    }
+    
+    @Test
+    public void testGetIntMatrixParam() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/int/x;var=4000000"), "4000000");
+    }
+    
+    @Test
+    public void testGetIntMatrixParamWithFloatValueFails() throws ClientProtocolException, IOException
+    {
+        verifyInternalServerErrorResult(new RequestResult("get/matrix/typematch/int/x;var=123.45"));
+    }
+    
+    @Test
+    public void testGetShortMatrixParam() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/short/x;var=20000"), "20000");
+    }
+    
+    @Test
+    public void testGetLongMatrixParam() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/long/x;var=8000000000"), "8000000000");        
+    }
+    
+    @Test
+    public void testGetFloatMatrixParam() throws ClientProtocolException, IOException
+    {
+        float floatVal = 1234567890.1121314151f;
+        final RequestResult rr = new RequestResult(String.format("get/matrix/typematch/float/x;var=%f", floatVal));
+        assertEquals(Status.OK.getStatusCode(), rr.status());
+        assertEquals(Status.OK.getReasonPhrase(), rr.reason());
+        assertEquals(Float.valueOf(floatVal), Float.valueOf(rr.content()));
+    }
+    
+    @Test
+    public void testGetDoubleMatrixParam() throws ClientProtocolException, IOException
+    {
+        double doubleVal = 102030405060708090.019181716151413121;
+        final RequestResult rr = new RequestResult(String.format("get/matrix/typematch/double/x;var=%f", doubleVal));
+        assertEquals(Status.OK.getStatusCode(), rr.status());
+        assertEquals(Status.OK.getReasonPhrase(), rr.reason());
+        assertEquals(Double.valueOf(doubleVal), Double.valueOf(rr.content()));
+    }
+    
+    @Test
+    public void testGetDoubleMatrixParamWithIntValueConverts() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/double/x;var=13579"), "13579.0");
+    }
+    
+    @Test
+    public void testGetByteMatrixParam() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/byte/x;var=127"), "127");        
+    }
+    
+    @Test
+    public void testGetBooleanMatrixParam() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/boolean/x;var=true"), "true");        
+        verifyOkResult(new RequestResult("get/matrix/typematch/boolean/x;var=false"), "false");        
+        verifyOkResult(new RequestResult("get/matrix/typematch/boolean/x;var=True"), "true");        
+        verifyOkResult(new RequestResult("get/matrix/typematch/boolean/x;var=FALSE"), "false");        
+    }
+    
+    public void testGetStandardClassMatrixParamWithString() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/Integer/x;var=5"), "5");
+    }
+    
+    @Test
+    public void testGetCustomClassMatrixParamWithStringConstructor() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/customwithstringctor/x;var=test"), "test");
+    }
+    
+    @Test
+    public void testGetCustomClassMatrixParamWithStringViaValueOf() throws ClientProtocolException, IOException
+    {
+        verifyOkResult(new RequestResult("get/matrix/typematch/customvalueofstring/x;var=tset"), "tset");
+    }
+    
+    @Test
+    public void testGetCustomClassMatrixParamWithoutStringConversionFails() throws ClientProtocolException, IOException
+    {
+        verifyInternalServerErrorResult(new RequestResult("get/matrix/typematch/custominvalid/x;var=test"));
     }
     
     // Test conversion of outputs from native types, string, JSON-serializable classes to Response
--- rest/src/test/java/com/zoomulus/weaver/rest/RestServerTestResource.java
+++ rest/src/test/java/com/zoomulus/weaver/rest/RestServerTestResource.java
@@ -131,7 +131,7 @@ public class RestServerTestResource
     }
     
     @GET
-    @Path("get/matrix/multiple/{p1: first;1=one}/{p2: second;two=2")
+    @Path("get/matrix/multiple/{p1: first}/{p2: second}")
     public Response getMatrixParamMultiple(@PathParam("p1") final String p1,
             @MatrixParam("1") final String one,
             @MatrixParam("two") int two,
@@ -141,9 +141,86 @@ public class RestServerTestResource
     }
     
     @GET
-    @Path("get/matrix/multiple/{rep1}/{rep2}")
+    @Path("get/matrix/multiple/rep/{rep1}/{rep2}")
     public Response getMatrixParamRepeated(@MatrixParam("var") final String name)
     {
         return Response.status(Status.OK).entity("var:" + name).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/boolean/{booleanval}")
+    public Response getBooleanMatrixMatch(@MatrixParam("var") boolean b)
+    {
+        return Response.status(Status.OK).entity(Boolean.toString(b)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/byte/{byteval}")
+    public Response getByteMatrixMatch(@MatrixParam("var") byte b)
+    {
+        return Response.status(Status.OK).entity(Byte.toString(b)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/short/{shortval}")
+    public Response getShortMatrixMatch(@MatrixParam("var") short i)
+    {
+        return Response.status(Status.OK).entity(Short.toString(i)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/int/{intval}")
+    public Response getIntMatrixMatch(@MatrixParam("var") int i)
+    {
+        return Response.status(Status.OK).entity(Integer.toString(i)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/long/{longval}")
+    public Response getLongMatrixMatch(@MatrixParam("var") long l)
+    {
+        return Response.status(Status.OK).entity(Long.toString(l)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/float/{floatval}")
+    public Response getFloatMatrixMatch(@MatrixParam("var") float f)
+    {
+        return Response.status(Status.OK).entity(Float.toString(f)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/double/{doubleval}")
+    public Response getDoubleMatrixMatch(@MatrixParam("var") double d)
+    {
+        return Response.status(Status.OK).entity(Double.toString(d)).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/Integer/{intval}")
+    public Response getIntegerMatrixMatch(@MatrixParam("var") final Integer i)
+    {
+        return Response.status(Status.OK).entity(i).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/customwithstringctor/{value}")
+    public Response getCustomMatrixParamWithStringCtorMatch(@MatrixParam("var") final CustomWithStringCtor c)
+    {
+        return Response.status(Status.OK).entity(c.toString()).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/customvalueofstring/{value}")
+    public Response getCustomMatrixParamValueOfStringMatch(@MatrixParam("var") final CustomValueOfString c)
+    {
+        return Response.status(Status.OK).entity(c.toString()).build();
+    }
+    
+    @GET
+    @Path("get/matrix/typematch/custominvalid/{value}")
+    public Response getCustomMatrixParamInvalidMatch(@MatrixParam("var") final CustomInvalid c)
+    {
+        return Response.status(Status.OK).entity(c.toString()).build();
     }
 }
